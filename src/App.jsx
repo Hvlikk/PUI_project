c30999ba8d46d4de66067348208b263eaa24a877
Hvlikk <emiloszhalicki@gmail.com>
2025-04-09 00:31:19 +0200

    Added matchcard, dashboard, need to change css into sass

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar'; // Importujemy nasz komponent Navbar
+import Matchcard from './Matchcard';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <div className="App">
       {/* Wyświetlamy nasz pasek nawigacyjny */}
-      <Navbar />
-
+     <Navbar />      
       <div className="content">
-        <h1>Witaj w ScoreTracker!</h1>
+       <h1>Witaj w ScoreTracker!</h1>
       </div>
+      <Dashboard/>
+
     </div>
   );
 }
---
+++ src/Matchcard.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import './Matchcard.css'; 
+import { FaHome, FaTrophy, FaStar, FaNewspaper, FaTv, FaInfoCircle, FaEnvelope, FaMoon, FaSun, FaFutbol } from 'react-icons/fa'; // Dodajemy FaFutbol
+
+function Matchcard() {
+
+  
+    return (
+      <div className="match-card">
+        <div className="match-header">
+          <div className="match-league">
+            <span className="live-dot"></span>
+            Football • placeholder
+          </div>
+          <div className="star">★</div>
+        </div>
+  
+        <div className="match-body">
+          <div className="team">
+            <div className="team-logo"></div>
+            <div className="team-name">home-team</div>
+          </div>
+  
+          <div className="score-center">
+            <div className="score">2-1</div>
+            <div className="minute">12'</div>
+          </div>
+  
+          <div className="team">
+            <div className="team-logo"></div>
+            <div className="team-name">away-team</div>
+          </div>
+        </div>
+  
+        <div className="match-footer">
+          <div>Statistics</div>
+          <div>Line-ups</div>
+          <div className="mute">🔇</div>
+        </div>
+      </div>
+    );
+  }
+export default Matchcard;
